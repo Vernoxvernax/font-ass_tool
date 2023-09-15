4917d2de2015c986faffa5899cfc4b95a082158f
VernoxVernax <vernoxvernax@gmail.com>
2023-09-15 05:38:25 +0200

    support for windows; support for font names with spaces (lool); remove debugging code from lastest commit
    had to leave some old commented code behind because I'm lazy

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,23 @@ use std::process::{exit, ExitCode};
 use ass_deserialize::AssFont;
 use clap::{Arg, Command, ArgAction};
 use walkdir::WalkDir;
+
 use fontconfig_sys::{
-  FcMatchPattern, FcResultMatch, FcSetSystem, FcResult, FcChar8, FcBool, FcConfig, FcPattern, FcFontSet,
-  FcConfigSubstitute, FcDefaultSubstitute, FcPatternAddBool, FcFontSetAdd, FcPatternDuplicate, FcPatternGetString, FcFontSetSort,  FcPatternDestroy, FcConfigDestroy,
-  FcPatternCreate, FcPatternAddInteger, FcConfigBuildFonts, FcInitLoadConfig, FcFontSetCreate, FcConfigGetFonts, FcPatternGetBool, FcFontSetDestroy, FcWeightFromOpenType, FcSetApplication,
+  FcMatchPattern, FcResultMatch, FcSetSystem, FcResult, FcChar8, FcBool, FcConfig, FcPattern, FcFontSet, ffi_dispatch
 };
+
+#[cfg(not(windows))]
+use fontconfig_sys::{
+  FcConfigSubstitute, FcDefaultSubstitute, FcPatternAddBool, FcFontSetAdd, FcPatternDuplicate, FcPatternGetString, FcFontSetSort,  FcPatternDestroy, FcConfigDestroy,
+  FcPatternCreate, FcPatternAddInteger, FcConfigBuildFonts, FcInitLoadConfig, FcFontSetCreate, FcConfigGetFonts, FcPatternGetBool, FcFontSetDestroy, FcWeightFromOpenType,
+};
+
+#[cfg(windows)]
+use fontconfig_sys::statics::LIB;
 
 pub mod ass_deserialize;
 pub mod error;
+
 use crate::ass_deserialize::AssFile;
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 
@@ -68,15 +77,18 @@ fn main() -> ExitCode {
       let raw_files = to_file_list(files);
       let ass_files = deserialize(raw_files.clone());
       unsafe {
-        let config: *mut FcConfig = FcInitLoadConfig();
-        FcConfigBuildFonts(config);
+        // let config: *mut FcConfig = FcInitLoadConfig();
+        let config = ffi_dispatch!(LIB, FcInitLoadConfig,);
+        ffi_dispatch!(LIB, FcConfigBuildFonts, config);
+        // FcConfigBuildFonts(config);
         for (file, name) in ass_files.iter().zip(raw_files.iter()) {
           if let Err(err) = remux_this(find_font_files(file.clone(), config), name.clone(), force) {
             println!("Error occurred for {}:\n  {}", name, err);
             return ExitCode::FAILURE;
           };
         }
-        FcConfigDestroy(config);
+        ffi_dispatch!(LIB, FcConfigDestroy, config);
+        // FcConfigDestroy(config);
         return ExitCode::SUCCESS;
       }
     },
@@ -86,8 +98,8 @@ fn main() -> ExitCode {
       let raw_files = to_file_list(files);
       let ass_files = deserialize(raw_files.clone());
       unsafe {
-        let config: *mut FcConfig = FcInitLoadConfig();
-        FcConfigBuildFonts(config);
+        let config = ffi_dispatch!(LIB, FcInitLoadConfig,);
+        ffi_dispatch!(LIB, FcConfigBuildFonts, config);
         for (file, name) in ass_files.iter().zip(raw_files.iter()) {
           println!("{}:", name.clone());
           for font_file in find_font_files(file.clone(), config).fonts.iter() {
@@ -95,7 +107,7 @@ fn main() -> ExitCode {
           };
           println!();
         }
-        FcConfigDestroy(config);
+        ffi_dispatch!(LIB, FcConfigDestroy, config);
         return ExitCode::SUCCESS;
       }
     }
@@ -135,47 +147,75 @@ fn find_font_files(file: AssFile, config: *mut FcConfig) -> AssFile {
     };
 
     unsafe {
-      let pattern = FcPatternCreate() as *mut FcPattern;
+      let pattern = ffi_dispatch!(LIB, FcPatternCreate,);
+      // let pattern = FcPatternCreate() as *mut FcPattern;
       if pattern.is_null() {
         continue;
       }
 
-      FcPatternAddBool(pattern, FC_OUTLINE.as_ptr() as *mut c_char, true as FcBool);
-      FcPatternAddInteger(pattern, FC_SLANT.as_ptr() as *mut c_char, slant);
-      FcPatternAddInteger(pattern, FC_WEIGHT.as_ptr() as *mut c_char, FcWeightFromOpenType(weight));
+      ffi_dispatch!(LIB, FcPatternAddBool, pattern, FC_OUTLINE.as_ptr() as *mut c_char, true as FcBool);
+      ffi_dispatch!(LIB, FcPatternAddInteger, pattern, FC_SLANT.as_ptr() as *mut c_char, slant);
+      ffi_dispatch!(LIB, FcPatternAddInteger, pattern, FC_WEIGHT.as_ptr() as *mut c_char, ffi_dispatch!(LIB, FcWeightFromOpenType, weight));
+      // FcPatternAddBool(pattern, FC_OUTLINE.as_ptr() as *mut c_char, true as FcBool);
+      // FcPatternAddInteger(pattern, FC_SLANT.as_ptr() as *mut c_char, slant);
+      // FcPatternAddInteger(pattern, FC_WEIGHT.as_ptr() as *mut c_char, FcWeightFromOpenType(weight));
       
-      FcDefaultSubstitute(pattern);
-      if FcConfigSubstitute(config, pattern, FcMatchPattern) != 1 {
+      ffi_dispatch!(LIB, FcDefaultSubstitute, pattern);
+      // FcDefaultSubstitute(pattern);
+      if ffi_dispatch!(LIB, FcConfigSubstitute, config, pattern, FcMatchPattern) != 1 {
         continue;
       }
+      // if FcConfigSubstitute(config, pattern, FcMatchPattern) != 1 {
+      //   continue;
+      // }
 
-      let fset: *mut FcFontSet = FcFontSetCreate();
-      // fcfind(FcConfigGetFonts(config, FcSetApplication), fset, &family);
-      fcfind(FcConfigGetFonts(config, FcSetSystem), fset, &family);
+      let fset = ffi_dispatch!(LIB, FcFontSetCreate,);
+      fcfind(ffi_dispatch!(LIB, FcConfigGetFonts, config, FcSetSystem), fset, &family);
+      // let fset: *mut FcFontSet = FcFontSetCreate();
+      // // fcfind(FcConfigGetFonts(config, FcSetApplication), fset, &family);
+      // fcfind(FcConfigGetFonts(config, FcSetSystem), fset, &family);
       
       let result: *mut FcResult = &mut 0;
       let mut sets: *mut FcFontSet = { fset };
 
-      let matches: *mut FcFontSet = FcFontSetSort(config, &mut sets, 1, pattern, false as FcBool, std::ptr::null_mut(), result);
+      let matches = ffi_dispatch!(LIB, FcFontSetSort, config, &mut sets, 1, pattern, false as FcBool, std::ptr::null_mut(), result);
+      // let matches: *mut FcFontSet = FcFontSetSort(config, &mut sets, 1, pattern, false as FcBool, std::ptr::null_mut(), result);
       if (*matches).nfont == 0 {
         assfont.path = "Nothing found.".to_string();
         fonts.append(&mut vec![assfont]);
         continue;
       };
 
-      FcFontSetDestroy(fset);
-      FcPatternDestroy(pattern);
+      ffi_dispatch!(LIB, FcFontSetDestroy, fset);
+      ffi_dispatch!(LIB, FcPatternDestroy, pattern);
+      // FcFontSetDestroy(fset);
+      // FcPatternDestroy(pattern);
 
       let matching = *(*matches).fonts.offset(0);
       
       let mut file: *mut FcChar8 = &mut 0;
-      if FcPatternGetString(matching, FC_FILE.as_ptr() as *mut c_char, 0, &mut file) != FcResultMatch {
+      if ffi_dispatch!(LIB, FcPatternGetString, matching, FC_FILE.as_ptr() as *mut c_char, 0, &mut file) != FcResultMatch {
         continue;
       }
+      // if FcPatternGetString(matching, FC_FILE.as_ptr() as *mut c_char, 0, &mut file) != FcResultMatch {
+      //   continue;
+      // }
 
       assfont.path = std::str::from_utf8(CStr::from_ptr(file as *const c_char).to_bytes()).unwrap().to_owned();
+
+      if cfg!(windows) {
+        assfont.path = assfont.path.replace("/", "\\").replace("\\ ", " ");
+      }
+
+      assfont.path = if assfont.path.contains(' ') {
+        "\"".to_owned() + &assfont.path + "\""
+      } else {
+        assfont.path
+      };
+
       fonts.append(&mut vec![assfont]);
-      FcFontSetDestroy(matches);
+      ffi_dispatch!(LIB, FcFontSetDestroy, matches);
+      // FcFontSetDestroy(matches);
     }
   };
   AssFile { fonts }
@@ -188,12 +228,16 @@ fn fcfind(src: *mut FcFontSet, fset: *mut FcFontSet, family: &String) {
       let mut value = 0;
       let val: *mut FcBool = &mut value;
     
-      if FcPatternGetBool(pattern, FC_OUTLINE.as_ptr() as *mut c_char, 0, val) != FcResultMatch || *val != true as FcBool {
+      if ffi_dispatch!(LIB, FcPatternGetBool, pattern, FC_OUTLINE.as_ptr() as *mut c_char, 0, val) != FcResultMatch || *val != true as FcBool {
         continue;
-      };
+      }
+      // if FcPatternGetBool(pattern, FC_OUTLINE.as_ptr() as *mut c_char, 0, val) != FcResultMatch || *val != true as FcBool {
+      //   continue;
+      // };
       
       if pattern_match(pattern, FC_FULLNAME, family) || pattern_match(pattern, FC_FAMILY, family) {
-        FcFontSetAdd(fset, FcPatternDuplicate(pattern));
+        ffi_dispatch!(LIB, FcFontSetAdd, fset, ffi_dispatch!(LIB, FcPatternDuplicate, pattern));
+        // FcFontSetAdd(fset, FcPatternDuplicate(pattern));
       };
     }
   }
@@ -203,7 +247,8 @@ fn pattern_match(pat: *mut c_void, field: &'static [u8], name: &String) -> bool 
   unsafe {
     let mut str: *mut FcChar8 = null_mut();
     for index in 0.. {
-      if FcPatternGetString(pat, field.as_ptr() as *mut c_char, index, &mut str) == FcResultMatch {
+      if ffi_dispatch!(LIB, FcPatternGetString, pat, field.as_ptr() as *mut c_char, index, &mut str) == FcResultMatch {
+      // if FcPatternGetString(pat, field.as_ptr() as *mut c_char, index, &mut str) == FcResultMatch {
         let sstr: &String = &std::str::from_utf8(CStr::from_ptr(str as *const c_char).to_bytes()).unwrap().to_owned().to_lowercase();
         if name == sstr {
           return true;
@@ -237,13 +282,24 @@ fn remux_this(file: AssFile, name: String, force: bool) -> Result<(), String> {
       continue;
     }
     cmd = cmd.to_owned() + " -attach " + assfont.path.as_str();
-    if assfont.path.ends_with(".ttf") {
+
+    let path = assfont.path.replace("\"", "").to_lowercase();
+    if path.ends_with(".ttf") {
       cmd = cmd.to_owned() + " -metadata:s:" + track_index.to_string().as_str() + " mimetype=application/x-truetype-font";
-    } else if assfont.path.ends_with(".otf") {
+    } else if path.ends_with(".otf") {
       cmd = cmd.to_owned() + " -metadata:s:" + track_index.to_string().as_str() + " mimetype=application/x-font-opentype";
-    } else if assfont.path.ends_with(".ttc") {
+    } else if path.ends_with(".ttc") {
       cmd = cmd.to_owned() + " -metadata:s:" + track_index.to_string().as_str() + " mimetype=application/x-truetype-collection";
     }
+
+    let filename = if cfg!(windows) {
+      path.split('\\').last().unwrap()
+    } else {
+      path.split('/').last().unwrap()
+    };
+
+    cmd = cmd.to_owned() + " -metadata:s:" + track_index.to_string().as_str() + " filename=\"" + filename + "\"";
+
     track_index += 1;
     duppl_check.push_str(&assfont.path);
   };
@@ -255,14 +311,18 @@ fn remux_this(file: AssFile, name: String, force: bool) -> Result<(), String> {
     return Err("None of the required fonts could be found!".to_string());
   }
 
-  args.append(&mut cmd.split(' ').collect());
+  let vector_string = controlled_space_splitting(cmd);
+  let mut vector_str = vector_string.iter().map(|s| s.as_str()).collect();
+
+  args.append(&mut vector_str);
   args.append(&mut vec![&output]);
+  
   if force {
     args.append(&mut vec!["-y"])
   } else {
     args.append(&mut vec!["-n"]);
   }
-  println!("{:?}", args);
+
   let result = std::process::Command::new("ffmpeg").args(args).output().unwrap();
   match result.status.success() {
     true => Ok(()),
@@ -284,6 +344,30 @@ fn deserialize(files: Vec<String>) -> Vec<AssFile> {
     }
   };
   deserialized_files
+}
+
+fn controlled_space_splitting(input: String) -> Vec<String> {
+  let mut output: Vec<String> = vec![];
+  let mut temp: String = String::new();
+  let mut quotations = false;
+  for ch in input.chars() {
+    if ch == '\"' {
+      quotations = !quotations;
+    } else if ch == ' ' && ! quotations {
+      if !temp.is_empty() {
+        output.push(temp.clone());
+        temp.clear();
+      }
+    } else {
+      temp.push(ch);
+    }
+  }
+
+  if !temp.is_empty() {
+    output.push(temp);
+  }
+
+  output
 }
 
 fn to_file_list(input: Vec<String>) -> Vec<String> {
